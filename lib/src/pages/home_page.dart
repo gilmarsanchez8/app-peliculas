af6b6642e6011ad8575b8b9cab88c4902174e3c2
Gilmar Sanchez Cuartas <gsanchez@tsgroup.local>
2021-04-11 15:45:08 -0500

    Crear paginador para cargar peliculas horizontalmente

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatelessWidget {
   final peliculasProvider = new PeliculasProvider();
   @override
   Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
     return Scaffold(
         appBar: AppBar(
           title: Text('Películas en cines'),
@@ -47,15 +48,18 @@ class HomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text('Populares', style: Theme.of(context).textTheme.subtitle1)
-          ),
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text('Populares',
+                  style: Theme.of(context).textTheme.subtitle1)),
           SizedBox(height: 2.0),
-          FutureBuilder(
-            future: peliculasProvider.getPopulares(),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
               if (snapshot.hasData) {
-                return MovieHorizontal(peliculas: snapshot.data);
+                return MovieHorizontal(
+                  peliculas: snapshot.data,
+                  siguientePagina: peliculasProvider.getPopulares,
+                );
               } else {
                 return Center(child: CircularProgressIndicator());
               }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -8,6 +9,21 @@ class PeliculasProvider {
   String _apikey = '66e9cbb466258644b57dc1c883b873fd';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+
+  // ignore: deprecated_member_use
+  List<Pelicula> _populares = new List();
+
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -20,14 +36,25 @@ class PeliculasProvider {
   Future<List<Pelicula>> getEnCines() async {
     final url = Uri.https(_url, '3/movie/now_playing',
         {'api_key': _apikey, 'language': _language});
-    
+
     return await _procesarRespuesta(url);
   }
 
   Future<List<Pelicula>> getPopulares() async {
-    final url = Uri.https(_url, '3/movie/popular', 
-        {'api_key': _apikey, 'language': _language});
-    
-    return await _procesarRespuesta(url);
+    _popularesPage++;
+
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key': _apikey,
+      'language': _language,
+      'page': _popularesPage.toString()
+    });
+
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+
+    popularesSink(_populares);
+
+    return resp;
   }
 }
